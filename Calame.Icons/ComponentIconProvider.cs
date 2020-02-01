a14112f23a2a7fbb1485e66b26e6a5b0cbe8d3b9
ReMinoer <rmorillon@freneticexperience.com>
2020-02-01 11:32:36 +0100

    Update to latest Material Design icon pack and update components icons

--- Calame.Icons/ComponentIconProvider.cs
+++ Calame.Icons/ComponentIconProvider.cs
@@ -47,7 +47,7 @@ namespace Calame.Icons
             switch (glyphComponent)
             {
                 case ISceneNode _:
-                    return new IconDescription(PackIconMaterialKind.GoogleCirclesExtended, SceneGraphCategoryBrush);
+                    return new IconDescription(PackIconMaterialKind.AxisArrow, SceneGraphCategoryBrush);
                 case ICamera _:
                     return new IconDescription(PackIconMaterialKind.Video, SceneGraphCategoryBrush);
                 case IView _:
@@ -55,8 +55,8 @@ namespace Calame.Icons
                     
                 case IAnimationPlayer _:
                     return new IconDescription(PackIconMaterialKind.Animation, AnimationCategoryBrush);
-                //case AnimationGraph<> _:
-                //    return new IconDescription(PackIconMaterialKind.LockPattern, AnimationCategoryBrush);
+                case IAnimationGraph _:
+                    return new IconDescription(PackIconMaterialKind.LockPattern, AnimationCategoryBrush);
                 case Motion _:
                     return new IconDescription(PackIconMaterialKind.RunFast, AnimationCategoryBrush);
                 case MotorBase _:
@@ -69,7 +69,7 @@ namespace Calame.Icons
                 case SpriteTransformer _:
                     return new IconDescription(PackIconMaterialKind.Compare, GraphicsCategoryBrush);
                 case FillingRectangle _:
-                    return new IconDescription(PackIconMaterialKind.Fullscreen, GraphicsCategoryBrush);
+                    return new IconDescription(PackIconMaterialKind.CameraMeteringMatrix, GraphicsCategoryBrush);
                 case RendererBase _:
                     return new IconDescription(PackIconMaterialKind.ProjectorScreen, GraphicsCategoryBrush);
                     
@@ -85,31 +85,31 @@ namespace Calame.Icons
                 case CircleCollider _:
                     return new IconDescription(PackIconMaterialKind.VectorCircleVariant, PhysicsCategoryBrush);
                 case IGridCollider _:
-                    return new IconDescription(PackIconMaterialKind.Grid, PhysicsCategoryBrush);
+                    return new IconDescription(PackIconMaterialKind.ViewGridOutline, PhysicsCategoryBrush);
                 case ICollider _:
                     return new IconDescription(PackIconMaterialKind.VectorPolygon, PhysicsCategoryBrush);
 
                 case InteractiveRoot _:
                     return new IconDescription(PackIconMaterialKind.Layers, InputCategoryBrush);
                 case Controls _:
-                    return new IconDescription(PackIconMaterialKind.GamepadVariant, InputCategoryBrush);
+                    return new IconDescription(PackIconMaterialKind.GestureTap, InputCategoryBrush);
                     
                 case Actor _:
                     return new IconDescription(PackIconMaterialKind.AccountBox, ScriptingCategoryBrush);
                 case Trigger _:
-                    return new IconDescription(PackIconMaterialKind.AlertOctagram, ScriptingCategoryBrush);
+                    return new IconDescription(PackIconMaterialKind.AlertBoxOutline, ScriptingCategoryBrush);
                     
                 case Flipper _:
-                    return new IconDescription(PackIconMaterialKind.Sync, CoreCategoryBrush);
+                    return new IconDescription(PackIconMaterialKind.FlipHorizontal, CoreCategoryBrush);
                 case ILayerRoot _:
                     return new IconDescription(PackIconMaterialKind.Layers, CoreCategoryBrush);
                 case ILayerManager _:
-                    return new IconDescription(PackIconMaterialKind.Layers, CoreCategoryBrush);
+                    return new IconDescription(PackIconMaterialKind.LayersTriple, CoreCategoryBrush);
                     
                 case FreeCamera _:
                     return new IconDescription(PackIconMaterialKind.VideoSwitch, ToolCategoryBrush);
                 case SceneNodeEditor _:
-                    return new IconDescription(PackIconMaterialKind.ArrowAll, ToolCategoryBrush);
+                    return new IconDescription(PackIconMaterialKind.AxisArrow, ToolCategoryBrush);
                 case AreaComponentRenderer _:
                     return new IconDescription(PackIconMaterialKind.VectorRectangle, ToolCategoryBrush);
                 case ShapedComponentRendererBase _:
