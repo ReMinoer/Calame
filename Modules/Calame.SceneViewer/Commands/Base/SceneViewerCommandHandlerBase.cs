6705de18c0f4641341acfad18ea3b29d4b4d9357
ReMinoer <reminoer@gmail.com>
2020-11-26 00:16:35 +0100

    Add base commands for sessions

---
+++ Modules/Calame.SceneViewer/Commands/Base/SceneViewerCommandHandlerBase.cs
@@ -0,0 +1,16 @@
+﻿using Calame.Commands.Base;
+using Calame.SceneViewer.ViewModels;
+using Gemini.Framework.Commands;
+
+namespace Calame.SceneViewer.Commands.Base
+{
+    public abstract class SceneViewerCommandHandlerBase<TCommandDefinition> : DocumentCommandHandlerBase<SceneViewerViewModel, TCommandDefinition>
+        where TCommandDefinition : CommandDefinition
+    {
+        protected override bool CanRun(Command command, SceneViewerViewModel document)
+        {
+            return base.CanRun(command, document)
+                && document.Viewer.Runner?.Engine != null;
+        }
+    }
+}
---
+++ Modules/Calame.SceneViewer/Commands/Base/SessionCommandHandlerBase.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+using Calame.SceneViewer.ViewModels;
+using Gemini.Framework.Commands;
+
+namespace Calame.SceneViewer.Commands.Base
+{
+    public abstract class SessionCommandHandlerBase<TSession, TCommandDefinition> : SceneViewerCommandHandlerBase<TCommandDefinition>
+        where TSession : class, ISession
+        where TCommandDefinition : CommandDefinition
+    {
+        protected override sealed bool CanRun(Command command, SceneViewerViewModel document)
+        {
+            return base.CanRun(command, document)
+                && document.Session is TSession session
+                && CanRun(command, session);
+        }
+
+        protected override sealed Task RunAsync(Command command, SceneViewerViewModel document)
+        {
+            if (document.Session is TSession activeSession)
+                return RunAsync(command, activeSession);
+
+            return Task.CompletedTask;
+        }
+
+        protected virtual bool CanRun(Command command, TSession session) => true;
+        protected abstract Task RunAsync(Command command, TSession session);
+    }
+}
