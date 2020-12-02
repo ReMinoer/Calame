d8b13c1a8c98fa634441ffb4ca09154cc3157bb5
ReMinoer <reminoer@gmail.com>
2020-12-02 21:42:50 +0100

    Add icons for demo editors and sessions

---
+++ Demos/Calame.Demo/Modules/DemoGameData/EditorIconDescriptor.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel.Composition;
+using System.Windows.Media;
+using Calame.DataModelViewer;
+using Calame.Icons;
+using Calame.Icons.Base;
+using Diese;
+using MahApps.Metro.IconPacks;
+
+namespace Calame.Demo.Modules.DemoGameData
+{
+    [Export(typeof(IIconDescriptorModule))]
+    [Export(typeof(IIconDescriptorModule<IEditorSource>))]
+    [Export(typeof(ITypeIconDescriptorModule))]
+    [Export(typeof(ITypeIconDescriptorModule<IEditorSource>))]
+    public class EditorIconDescriptor : TypeIconDescriptorModuleBase<IEditorSource>
+    {
+        static public readonly Brush DefaultBrush = Brushes.DimGray;
+
+        public override IconDescription GetTypeIcon(Type type)
+        {
+            if (type.Is<SceneEditorSource>())
+                return new IconDescription(PackIconMaterialKind.Group, DefaultBrush);
+            if (type.Is<RectangleEditorSource>())
+                return new IconDescription(PackIconMaterialKind.VectorRectangle, DefaultBrush);
+            if (type.Is<CircleEditorSource>())
+                return new IconDescription(PackIconMaterialKind.VectorCircleVariant, DefaultBrush);
+
+            return IconDescription.None;
+        }
+    }
+}
---
+++ Demos/Calame.Demo/Modules/DemoGameData/SessionIconDescriptor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel.Composition;
+using System.Windows.Media;
+using Calame.Icons;
+using Calame.Icons.Base;
+using Calame.SceneViewer;
+using Diese;
+using MahApps.Metro.IconPacks;
+
+namespace Calame.Demo.Modules.DemoGameData
+{
+    [Export(typeof(IIconDescriptorModule))]
+    [Export(typeof(IIconDescriptorModule<ISession>))]
+    [Export(typeof(ITypeIconDescriptorModule))]
+    [Export(typeof(ITypeIconDescriptorModule<ISession>))]
+    public class SessionIconDescriptor : TypeIconDescriptorModuleBase<ISession>
+    {
+        static public readonly Brush DefaultBrush = Brushes.DimGray;
+
+        public override IconDescription GetTypeIcon(Type type)
+        {
+            if (type.Is<MovingSession>())
+                return new IconDescription(PackIconMaterialKind.Gamepad, DefaultBrush);
+
+            return IconDescription.None;
+        }
+    }
+}
