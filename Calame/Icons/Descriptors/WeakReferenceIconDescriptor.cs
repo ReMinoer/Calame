50e080330cc8fc8dcc9a06d45bc72eb6d1284dd2
ReMinoer <reminoer@gmail.com>
2020-12-15 00:04:06 +0100

    Add retargeting icon descriptor for Nullable and WeakReference

---
+++ Calame/Icons/Descriptors/WeakReferenceIconDescriptor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.Composition;
+using Calame.Icons.Base;
+
+namespace Calame.Icons.Descriptors
+{
+    [Export(typeof(IIconDescriptorModule))]
+    [Export(typeof(ITypeIconDescriptorModule))]
+    public class WeakReferenceIconDescriptor : TypeReTargetingDescriptorModuleBase
+    {
+        public override bool Handle(Type type) => type != null && type.IsGenericType && type.GetGenericTypeDefinition() == typeof(WeakReference<>);
+        protected override Type GetTypeTarget(Type type) => type.GenericTypeArguments[0];
+    }
+}
