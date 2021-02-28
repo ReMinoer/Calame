ba9cb6c2c315d7f2ea19abd91fcb1ba0e8a9a243
ReMinoer <reminoer@gmail.com>
2021-02-28 10:48:02 +0100

    Update Glyph

--- Calame.Icons/Descriptors/GlyphTypesIconDescriptor.cs
+++ Calame.Icons/Descriptors/GlyphTypesIconDescriptor.cs
@@ -5,6 +5,7 @@ using Calame.Icons.Base;
 using Diese;
 using Glyph.Animation;
 using Glyph.Core;
+using Glyph.Core.Schedulers;
 using Glyph.Graphics;
 using Glyph.IO;
 using Glyph.Math;
@@ -106,7 +107,7 @@ namespace Calame.Icons.Descriptors
             if (type.Is<IAnimation>())
                 return new IconDescription(PackIconMaterialKind.Animation, DefaultBrush);
 
-            if (type.Is<IGlyphScheduler>() || type.Is<SchedulerHandler>())
+            if (type.Is<IGlyphScheduler>() || type.Is<ComponentSchedulerHandler>())
                 return new IconDescription(PackIconMaterialKind.CalendarRefresh, DefaultBrush);
             if (type.Is<IRouter>())
                 return new IconDescription(PackIconMaterialKind.RouterNetwork, DefaultBrush);
